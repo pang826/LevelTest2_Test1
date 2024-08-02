c5d1e7ec34e541a243031ad41833560750eb52b7
ogh826 <eys555@naver.com>
2024-08-02 11:01:09 +0900

    Test1

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Specialized;
+
+namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iLeft = 10;
+            int iRight = 20;
+            Util.Swap(ref iLeft, ref iRight);
+            Console.WriteLine("int 자료형을 사용한 Swap 함수");
+            Console.WriteLine($"{iLeft},{iRight}");
+
+            Console.WriteLine();
+
+            double dLeft = 3.5;
+            double dRight = 8.8;
+            Util.Swap(ref dLeft, ref dRight);
+            Console.WriteLine("double 자료형을 사용한 Swap 함수");
+            Console.WriteLine($"{dLeft},{dRight}");
+        }
+        public class Util
+        {
+            static public void Swap<T>(ref T a, ref T b) where T : struct
+            {
+                T temp = a;
+                a = b;
+                b = temp;
+            }
+        }
+    }
+
+    
+}
